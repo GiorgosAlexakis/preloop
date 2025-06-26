9188c7c319b774a657bc014cce8625016c335873
Yannis Chatzikonstantinou <info@tinymovr.com>
2025-06-26 08:07:54 +0200

    add static to vite

--- lib/frontend/vite.config.ts
+++ lib/frontend/vite.config.ts
@@ -24,6 +24,11 @@ export default defineConfig({
         changeOrigin: true,
         ws: true,
       },
+      '/static': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        ws: true,
+      },
     },
   },
 });
